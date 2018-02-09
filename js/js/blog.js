0b48bd0e5e1c70030a182c8a120efffa8d3005d2
a0929377976 <35485004+a0929377976@users.noreply.github.com>
2018-02-09 18:22:02 +0800

    0209-1821

---
+++ js/js/blog.js
@@ -0,0 +1,7 @@
+$('.page-scroll a').click(function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+});
